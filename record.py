4225e218aa97829d02ee8d8cd882f5166be11f42
xiaobo <55168188+xiao-bo-8@users.noreply.github.com>
2019-10-27 22:52:34 +0800

    Add files via upload

--- record.py
+++ record.py
@@ -1,47 +1,38 @@
 import pyaudio
 import wave
-import os
 
-# import numpy as np
-# f = open("sw02725-A_018202-018583#sw02141-B_013798-014064.wav")
-# f.seek(0)
-# f.read(44)
-# data = np.fromfile(f, dtype=np.int16)
-# data.tofile("test.pcm")
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "test.wav"
 
-def record():
-    CHUNK = 16
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 16000
-    RECORD_SECONDS = 4
-    WAVE_OUTPUT_FILENAME = "output.wav"
+p = pyaudio.PyAudio()
 
-    p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
 
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
+print("开始录音......")
 
-    print("* recording")
+frames = []
 
-    frames = []
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
+print("录音结束!")
 
-    print("* done recording")
+stream.stop_stream()
+stream.close()
+p.terminate()
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
